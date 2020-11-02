ad7b1d4a62bfc49a7f079a6df0ac6508f43c46dd
Paul <49080975+pav311@users.noreply.github.com>
2020-11-03 00:34:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import random
 import time
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-vk_session = vk_api.VkApi(token="")
+vk_session = vk_api.VkApi(token="") #тут токен
 longpoll = VkLongPoll(vk_session)
 vk = vk_session.get_api()
 
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-        if event.user_id==508378083:
+        if event.user_id==1:# тут айди
             vk.messages.send(chat_id=event.chat_id,
                 message=random.choice(("ХОХЛА НЕ СПРАШИВАЛИ",
                     "хохол проснулся",
@@ -23,9 +23,7 @@ for event in longpoll.listen():
                 reply_to=event.message_id,
                 random_id=random.randint(1,999999)
             )
-            #time.sleep(1) or event.user_id==288146505 or event.user_id==395572016 or event.user_id==560302519
         elif  "не хохол" in event.text.lower() or "крым" in event.text.lower() or "украин" in event.text.lower() or "хрю" in event.text.lower() or "либер" in event.text.lower():
-            #vk.messages.setActivity(type="typing", peer_id=event.chat_id)
             vk.messages.send(chat_id=event.chat_id, 
                 message=random.choice(("ХОХЛА НЕ СПРАШИВАЛИ",
                     "хохол проснулся",
